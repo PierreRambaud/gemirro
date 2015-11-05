21bae66f46b87154bf5aa6768e28821b8f947825
Pierre RAMBAUD <pierre.rambaud@numergy.com>
2015-11-05 13:14:00 +0100

    Do not cache empty or nil value

--- lib/gemirro/cache.rb
+++ lib/gemirro/cache.rb
@@ -102,6 +102,7 @@ module Gemirro
     # @return [Mixed]
     #
     def write(key_hash, value)
+      return value if value.nil? || value.empty?
       File.open(key_path(key_hash), 'wb') do |f|
         Marshal.dump(value, f)
       end
