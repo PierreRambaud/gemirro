c057e6c03ec0e853f2bc25b8aba54a607609d99d
Pierre RAMBAUD <pierre.rambaud@numergy.com>
2015-12-03 14:27:01 +0100

    Fixed #24 add force option to allow overwriting

--- lib/gemirro/cli/init.rb
+++ lib/gemirro/cli/init.rb
@@ -3,18 +3,23 @@ Gemirro::CLI.options.command 'init' do
   banner 'Usage: gemirro init [DIRECTORY] [OPTIONS]'
   description 'Sets up a new mirror'
   separator "\nOptions:\n"
+  on :force, 'Force overwrite'
 
-  run do |_opts, args|
+  run do |opts, args|
     directory = File.expand_path(args[0] || Dir.pwd)
     template  = Gemirro::Configuration.template_directory
 
     Dir.mkdir(directory) unless File.directory?(directory)
 
-    Dir.glob("#{template}/**/*", File::FNM_DOTMATCH).each do |file|
-      next if ['.', '..'].include?(File.basename(file))
-      dest = File.join(directory, file.gsub(/^#{template}/, ''))
-      next if File.exist?(dest)
-      FileUtils.cp_r(file, dest)
+    if opts[:force]
+      FileUtils.cp_r(File.join(template, '.'), directory)
+    else
+      Dir.glob("#{template}/**/*", File::FNM_DOTMATCH).each do |file|
+        next if ['.', '..'].include?(File.basename(file))
+        dest = File.join(directory, file.gsub(/^#{template}/, ''))
+        next if File.exist?(dest)
+        FileUtils.cp_r(file, dest)
+      end
     end
 
     puts "Initialized empty mirror in #{directory}"
