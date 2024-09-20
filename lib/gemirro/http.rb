a5bb6028869ec8092c8fec5c12ab48c47899da7f
Bin Li <bli111@bloomberg.net>
2024-09-20 14:42:40 -0400

    add authentication,selfsigned cert,proxy support

--- lib/gemirro/http.rb
+++ lib/gemirro/http.rb
@@ -29,7 +29,35 @@ module Gemirro
     # @return [HTTPClient]
     #
     def self.client
-      @client ||= HTTPClient.new
+      client ||= HTTPClient.new
+      config = Utils.configuration
+      if defined?(config.upstream_user)
+        user = config.upstream_user
+        password = config.upstream_password
+        domain = config.upstream_domain
+        client.set_auth(domain, user, password)
+      end
+
+      if defined?(config.proxy)
+        proxy = config.proxy
+        client.proxy=(proxy)
+      end
+
+      # Use my own ca file for self signed cert
+      if defined?(config.rootca)
+          abort "The configuration file #{config.rootca} does not exist" unless File.file?(config.rootca)
+          client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_PEER
+          client.ssl_config.set_trust_ca(config.rootca)
+      elsif defined?(config.verify_mode)
+        client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE unless config.verify_mode
+      end
+
+      # Enforece base auth
+      if defined?(config.basic_auth)
+        client.force_basic_auth=(true) if config.basic_auth
+        # client.www_auth.reset_challenge()
+      end
+      @client = client
     end
   end
 end
